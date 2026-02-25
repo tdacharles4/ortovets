a3eda948a00e4d37d8139e97b2053ea735981821
adiosulises <3adiosulises@gmail.com>
2026-02-24 19:08:26 -0700

    cambio del link otra vez

--- app/page.tsx
+++ app/page.tsx
@@ -206,7 +206,7 @@ export default async function Home() {
             <p>Not logged in</p>
           )}
 
-          <a href="https://shopify.com/97515667777/account/login?return_to=https://davidpaz.org" className="underline btn">
+          <a href="https://shopify.com/97515667777/account/login?return_to=https%3A%2F%2Fdavidpaz.org" className="underline btn">
             Login
           </a>
         </div>
